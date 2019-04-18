26a7f33b9baa08b2939daf705d9dc8e73624eceb
ositaigwe@pursuit.org <j0h@example.com>
2019-04-17 21:20:05 -0400

    initial coding of app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {HashRouter, Switch, Route} from 'react-router-dom';
+
+//PAGE
+  //HOME
+  //MASTER USER LIST
+  //USER PROFILE PAGE
+  //USER ADD SHOW
+  //SPECIFIC SHOW
+  //MASTER SHOWS LIST
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <HashRouter>
+          <Switch>
+            <Route path='/'/>
+            <Route path='/users' exact/>
+            <Route path='/users:id' />
+            <Route path='/user/post'/>
+            <Route path='/shows' exact />
+            <Route path='/show/:id' />
+          </Switch>
+        </HashRouter>
     );
   }
 }
