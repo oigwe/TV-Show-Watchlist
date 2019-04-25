a7c80c9f518fc350fc256196c210e22cce7d54af
ositaigwe@pursuit.org <j0h@example.com>
2019-04-24 23:55:31 -0400

    removed links from comments

--- frontend/src/components/comments.js
+++ frontend/src/components/comments.js
@@ -9,8 +9,7 @@ const Comments = (props) => {
                     return <div className="media mt-3" key={i}>
                         <img className="mr-3" src="http://2.gravatar.com/avatar/81304e8f5a63d0fb806ba18eff525f0f?s=49&d=mm&r=g" alt="profile" />
                         <div className="media-body">
-                            <Link to={`/user/${e.user_id}`} style={{ color: "black" }}><h5 className="mt-0">{e.username}</h5></Link>
-                            {e.comment_body}
+                            <h5 className="mt-0">{e.username}</h5>
                         </div>
                     </div>
                 })
