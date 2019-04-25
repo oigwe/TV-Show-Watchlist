154a5a1d8feff6a4ca92b160a1a2a57bffce06c2
ositaigwe@pursuit.org <j0h@example.com>
2019-04-25 00:02:57 -0400

    accidently deleted comment body readded

--- frontend/src/components/comments.js
+++ frontend/src/components/comments.js
@@ -9,9 +9,11 @@ const Comments = (props) => {
                     return <div className="media mt-3" key={i}>
                         <img className="mr-3" src="http://2.gravatar.com/avatar/81304e8f5a63d0fb806ba18eff525f0f?s=49&d=mm&r=g" alt="profile" />
                         <div className="media-body">
-                            <h5 className="mt-0">{e.username}</h5>
+                        <h5 className="mt-0">{e.username}</h5>
+
+                            {e.comment_body}
                         </div>
-                    </div>
+                        </div>
                 })
             }
         </div>
